2dcca1f309b09aff0468b9a5b224f769a1a366bc
Vera Weikel <vera.weikel@gmail.com>
2023-02-19 20:23:31 -0800

    Adds Program code.

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -7,7 +7,9 @@ namespace ShapeTracker
   {
     static void Main()
     {
-
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Shape Tracker app!");
+      Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
     }
   }
 }
