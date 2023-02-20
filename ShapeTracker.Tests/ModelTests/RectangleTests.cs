7ece176834c6b56152a20a0d25f81acca43efd2d
Vera Weikel <vera.weikel@gmail.com>
2023-02-20 14:17:33 -0800

    Adds test scaffoldings for all shapes.

---
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class RectangleTests
+  {
+    // Tests will go here!
+  }
+}
