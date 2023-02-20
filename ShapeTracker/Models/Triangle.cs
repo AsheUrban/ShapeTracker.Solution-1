6a79978456722f63869bd1d270895c41041ae6e8
Vera Weikel <vera.weikel@gmail.com>
2023-02-19 18:21:00 -0800

    Create 3 fields to hold the three integer values that we'll use to determine the type of a triangle.

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -3,8 +3,8 @@ namespace ShapeTracker.Models
 {
   public class Triangle
   {
-    // static void TiangleSol() 
-    // c# pgm code goes here.
-    // Console.WriteLine("Hello Triangle's business logic code"); 
+    public int Side1;
+    public int Side2 = 4; 
+    public int Side3; 
   }
 }
