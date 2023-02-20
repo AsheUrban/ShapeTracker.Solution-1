5fab70c189e871da3201989f18de15558cf9f742
Vera Weikel <vera.weikel@gmail.com>
2023-02-19 17:53:52 -0800

    Add main folder.

---
+++ ShapeTracker/Models/Triangle.cs
@@ -0,0 +1,8 @@
+// using System;
+
+public class Triangle
+{
+  // static void TiangleSol() 
+  // c# pgm code goes here.
+  // Console.WriteLine("Hello Triangle's business logic code"); 
+}
---
+++ ShapeTracker/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+  static void Main() 
+  {
+    // c# pgm UI code goes here.
+    Console.WriteLine("Hello World! This is required to have one entry point called Main() in our project's code"); 
+  }
+}
