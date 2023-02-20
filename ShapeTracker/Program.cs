4be37516d5b6db0dda9373fb0749d5503f3d7035
Vera Weikel <vera.weikel@gmail.com>
2023-02-19 20:22:21 -0800

    Clears Program code.

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using ShapeTracker.Models;
-using System.Collections.Generic;  // new using directive!
 
 namespace ShapeTracker
 {
@@ -8,24 +7,7 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      List<Triangle> allTriangles = Triangle.GetAll();
-      Triangle testTriangle = new Triangle(3,4,5);
-      Triangle secondTriangle = new Triangle(32,74,75);
-      if (allTriangles.Count == 0)
-      {
-        Console.WriteLine("There are no triangles!");
-      }
-      else
-      {
-        Console.WriteLine("----------------------------------");
-        foreach (Triangle tri in allTriangles)
-        {
-          Console.WriteLine($"Side one of the triangle: {tri.GetSide1()}");
-          Console.WriteLine($"Side two of the triangle: {tri.GetSide2()}");
-          Console.WriteLine($"Side three of the triangle: {tri.GetSide3()}");
-          Console.WriteLine("----------------------------------");
-        }
-      }
+
     }
   }
 }
