d40fab8c0ea9eaea069cb687ffa4a193f38d5404
Vera Weikel <vera.weikel@gmail.com>
2023-02-20 15:08:22 -0800

    Tests both pass: AAA: arrange, act, assert test for our first test(again).
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,11 +6,10 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
-
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(2);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
