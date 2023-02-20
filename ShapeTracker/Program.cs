e3e59c254ed597cbb6973f09e4965b940b8b14f0
Vera Weikel <vera.weikel@gmail.com>
2023-02-19 20:24:28 -0800

    Adds Program code to Write/Read lines for parsed triangle lengths.

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -10,6 +10,16 @@ namespace ShapeTracker
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+      Console.WriteLine("Please enter a number:");
+      string stringNumber1 = Console.ReadLine();  
+      Console.WriteLine("Enter another number:");
+      string stringNumber2 = Console.ReadLine();  
+      Console.WriteLine("Enter a third number:");
+      string stringNumber3 = Console.ReadLine();  
+      int length1 = int.Parse(stringNumber1);  
+      int length2 = int.Parse(stringNumber2);  
+      int length3 = int.Parse(stringNumber3);  
+      Triangle tri = new Triangle(length1, length2, length3);
     }
   }
 }
