4dd36b788c975b8a4ab18875468d6cb9bd666e86
Vera Weikel <vera.weikel@gmail.com>
2023-02-20 15:03:13 -0800

    AAA: arrange, act, assert test.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -14,5 +14,16 @@ namespace ShapeTracker.Tests
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      // Arrange
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      // Act (by calling a method or retreiving a property) ie. Side1
+      int result = newTriangle.Side1;
+      // Assert
+      Assert.AreEqual(length1, result);
+    }
   }
 }
