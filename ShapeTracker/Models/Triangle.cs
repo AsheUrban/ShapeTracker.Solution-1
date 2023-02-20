879f2ff3f6e45796570e528d2bdc7b7856ff53cf
Vera Weikel <vera.weikel@gmail.com>
2023-02-20 15:30:43 -0800

    Pass test Side 2: testing for auto-implemented properties.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -11,7 +11,7 @@ namespace ShapeTracker.Models
       set { _side1 = value; }
     }
 
-    public int Side2 { get; }
+    public int Side2 { get; set; }
 
     public Triangle(int length1, int length2)
     {
