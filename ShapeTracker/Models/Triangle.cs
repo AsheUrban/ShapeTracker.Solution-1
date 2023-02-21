5c10a4e3c1a320553841a1a2892bc66b0ca1e97a
Vera Weikel <vera.weikel@gmail.com>
2023-02-20 17:52:00 -0800

    Add flexibility of parameters in a public Triangle() {} overloaded constructor.

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -15,7 +15,12 @@ namespace ShapeTracker.Models
     private int _side3; 
     private static List<Triangle> _instances = new List<Triangle> {};
     // auto-implemented property (like Side2)
-    public string Guess { get; set; }
+    
+    // public string Guess { get; set; }
+    
+    // Data type of parameters: If we want to allow users to enter an open-ended guess, like entering the string "isosceles", or make a selection from a predefined list, like choosing the char 'a' or 'b', we can add overloaded constructors to account for different type of data. Note that you will need separate properties/fields for each type of data you want to store.
+    public string OpenGuess { get; set; }
+    public char MultipleChoiceGuess { get; set; }
     public Triangle(int length1, int length2, int length3)
     {
       _side1 = length1;
@@ -24,16 +29,27 @@ namespace ShapeTracker.Models
       _instances.Add(this);
     }
 
+    // Flexibility of parameters: We can choose to have a different number of parameters (as seen in the example we use above) or even no parameters at all. To include no parameters, add an empty constructor like this:
+    // this may also be written as: 
+    public Triangle() {}
+
     // DRY up constructor overload
     public Triangle(int length1, int length2, int length3, string userGuess): this (length1, length2, length3)
     {
       // _side1 = length1; 
       // Side2 = length2;
       // _side3 = length3;
-      Guess = userGuess; 
+      // Guess = userGuess; 
       // _instances.Add(this);
+
+      OpenGuess = userGuess; 
+
     }
 
+    public Triangle(int length1, int length2, int length3, char userGuess) : this(length1, length2, length3)
+    {
+      MultipleChoiceGuess = userGuess; 
+    }
     public int GetSide3()
     {
       return _side3;
